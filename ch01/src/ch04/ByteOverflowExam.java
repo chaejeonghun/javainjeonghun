b07c280d800dcebb70ba06e8351f372e220f0474
채정훈 <@DESKTOP-PMC652T.local>
2021-09-17 12:44:32 +0900

    dqdwq

---
+++ ch01/src/ch04/ByteOverflowExam.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class ByteOverflowExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = -128; i < 500; i++) {
+			System.out.println((byte) i);
+		}
+	}
+
+}
