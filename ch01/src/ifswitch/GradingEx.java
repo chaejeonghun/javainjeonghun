9e3795fb5c17fb32c7ec61fecd0ee921b0ae0315
채정훈 <@DESKTOP-PMC652T.local>
2021-09-23 17:06:30 +0900

    ㅇㅇㅇ

---
+++ ch01/src/ifswitch/GradingEx.java
@@ -0,0 +1,32 @@
+package ifswitch;
+
+import java.util.Scanner;
+
+//다중 if-else 문
+public class GradingEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char grade;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("점수를 입력하세요 (0~100) : ");
+		int score = scanner.nextInt();
+
+		if (score >= 90)
+			grade = 'A';
+		else if (score >= 80)
+			grade = 'B';
+		else if (score >= 70)
+			grade = 'C';
+		else if (score >= 60)
+			grade = 'D';
+		else
+			grade = 'F';
+
+		System.out.println("학점은 " + grade + "입니다.");
+		// 자원해제
+		scanner.close();
+	}
+
+}
---
+++ ch01/src/ifswitch/MultipleofThreeExam.java
@@ -0,0 +1,22 @@
+package ifswitch;
+
+import java.util.Scanner;
+
+public class MultipleofThreeExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.println(" 수를 입력하시오 : ");
+		int number = in.nextInt();
+
+		if (number % 3 == 0)
+			System.out.println("3 의 배수입니다.");
+		else
+			System.out.println("3의 배수가 아닙니다.");
+
+	}
+
+}
