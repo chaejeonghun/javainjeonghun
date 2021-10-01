03252b30289619d5a294b2a2df70dbfe81661d91
채정훈 <@DESKTOP-PMC652T.local>
2021-10-01 12:16:01 +0900

    ㅁ

---
+++ ch01/src/Today1001af/A.java
@@ -0,0 +1,16 @@
+package Today1001af;
+
+//소스 파일명과 public 으로 선언된 class 이름은 같아야 한다.
+
+public class A {
+
+}
+
+//소스파일 명과 다른 클래스에 public으로 선언하면 오류 발생
+// public class B{} - 오류 발생
+class C {
+} // 오류 없음
+
+/* 
+ * 
+ */
---
+++ ch01/src/Today1001af2/A.java
@@ -0,0 +1,14 @@
+package Today1001af2;
+
+//하나의 소스파일에 여러개의 클래스 선언가능
+//소스파일은 하나 - 컴파일된 클래스는 3 개
+//소스 파일명과 public 으로 선언된 class 이름은 같아야 한다.
+public class A {
+
+}
+
+class B {
+}
+
+class C {
+}
