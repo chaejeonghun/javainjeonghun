3722ef45ccf9e08b7b71dc23dec74c08b35c57aa
채정훈 <@DESKTOP-PMC652T.local>
2021-10-01 12:33:18 +0900

    dqdq

---
+++ ch01/src/Today1001af/Car.java
@@ -0,0 +1,16 @@
+package Today1001af;
+
+//소스코드
+public class Car {
+
+}
+
+/*
+ * 생성자 - 클래스의 접근 제한자 class 클래스명(){}
+ * 
+ * 소스코드에 기본 생성자가 없으면 컴파일러가 컴파일시 기본 생성자 추가
+ * 
+ * 
+ * 
+ * 컴파일 후 public class Car{ public class Car(){} }
+ */
---
+++ ch01/src/Today1001af/UseCar.java
@@ -0,0 +1,13 @@
+package Today1001af;
+
+public class UseCar {
+
+	public static void main(String[] args) {
+		// Car 객체 생성
+		// new Car(); 호출
+		Car car = new Car();
+		// 소스코드로 부터 컴파일된 클래스 Car 클래스 파일로 부터 객체 생성
+
+	}
+
+}
